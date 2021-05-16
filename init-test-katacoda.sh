9a680f407b8a5fdbac63c591c04726d07a74575d
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2021-05-16 10:31:19 +0700

    Update init-test-katacoda.sh

--- init-test-katacoda.sh
+++ init-test-katacoda.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+git clone https://github.com/paichayon321/python-upload2.git
+cd python-upload2
+docker pull python:3.9.5
+docker run --privileged -p 30000:5000 --rm -e var1="value1" -it --volume "$(pwd):/app" python:3.9.5 bash -c "pip install flask && python /app/upload.py"
